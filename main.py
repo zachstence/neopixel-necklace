2793c8754d4510b07efab3b86213f7e18b1acafb
Zach Stence <zachstence@gmail.com>
2023-02-19 01:21:08 -0600

    feat: NeoPixelRange Thank you todbot https://www.reddit.com/r/circuitpython/comments/v7etu3/pixel_map_on_certain_areas_of_neopixels/

--- main.py
+++ main.py
@@ -8,10 +8,28 @@ import board
 from rainbowio import colorwheel
 import neopixel
 
+class NeoPixelRange:
+    def __init__(self, neopixel_obj, start, end):
+        self._neopixel_obj = neopixel_obj
+        self.start = start
+        self.end = end
+    def fill(self, color):
+        for i in range(self.start, self.end):
+            self._neopixel_obj[i] = color
+    def show(self):
+        self._neopixel_obj.show()
+    def __len__(self):
+        return (self.end - self.start)
+    def __setitem__(self, key, color):
+        self._neopixel_obj[self.start+key] = color
+
 PIN = board.D1
 NUM_PIXELS = 23
 
-pixels = neopixel.NeoPixel(PIN, NUM_PIXELS, brightness=0.03, auto_write=False)
+PIXELS = neopixel.NeoPixel(PIN, NUM_PIXELS, brightness=0.03, auto_write=False)
+OUTER_RING = NeoPixelRange(PIXELS, 0, 16)
+INNER_RING = NeoPixelRange(PIXELS, 17, 23)
+CENTER = NeoPixelRange(PIXELS, 16, 17)
 
 RED = (255, 0, 0)
 YELLOW = (255, 150, 0)
@@ -21,15 +39,30 @@ BLUE = (0, 0, 255)
 PURPLE = (180, 0, 255)
 OFF = (0, 0, 0, 0)
 
-active = 0
+outer_active = 0
+inner_active = 0
 
 while True:
-    for i in range(NUM_PIXELS):
-        if i == active:
-            pixels[i] = CYAN
+    for i in range(len(OUTER_RING)):
+        if i == outer_active:
+            OUTER_RING[i] = CYAN
         else:
-            pixels[i] = OFF
+            OUTER_RING[i] = OFF
     
-    pixels.show()
-    active = (active + 1) % NUM_PIXELS
+    outer_active = (outer_active + 1) % len(OUTER_RING)
+        
+    for i in range(len(INNER_RING)):
+        if i == inner_active:
+            INNER_RING[i] = PURPLE
+        else:
+            INNER_RING[i] = OFF
+    
+    if outer_active == 7:
+        CENTER[0] = BLUE
+    if outer_active == 15:
+        CENTER[0] = OFF
+
+    inner_active = (inner_active + 1) % len(INNER_RING)
+    
+    PIXELS.show()
     time.sleep(0.01)
