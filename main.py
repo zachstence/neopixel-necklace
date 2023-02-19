0d006c955dd07d6a95475908c1996eb7cdc62d16
Zach Stence <zachstence@gmail.com>
2023-02-19 01:05:30 -0600

    feat: custom chase code

--- main.py
+++ main.py
@@ -8,28 +8,10 @@ import board
 from rainbowio import colorwheel
 import neopixel
 
-pixel_pin = board.D1
-num_pixels = 23
+PIN = board.D1
+NUM_PIXELS = 23
 
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.03, auto_write=False)
-
-
-def color_chase(color, wait):
-    for i in range(num_pixels):
-        pixels[i] = color
-        time.sleep(wait)
-        pixels.show()
-    time.sleep(0.5)
-
-
-def rainbow_cycle(wait):
-    for j in range(255):
-        for i in range(num_pixels):
-            rc_index = (i * 256 // num_pixels) + j
-            pixels[i] = colorwheel(rc_index & 255)
-        pixels.show()
-        time.sleep(wait)
-
+pixels = neopixel.NeoPixel(PIN, NUM_PIXELS, brightness=0.03, auto_write=False)
 
 RED = (255, 0, 0)
 YELLOW = (255, 150, 0)
@@ -37,24 +19,17 @@ GREEN = (0, 255, 0)
 CYAN = (0, 255, 255)
 BLUE = (0, 0, 255)
 PURPLE = (180, 0, 255)
+OFF = (0, 0, 0, 0)
+
+active = 0
 
 while True:
-    pixels.fill(RED)
+    for i in range(NUM_PIXELS):
+        if i == active:
+            pixels[i] = CYAN
+        else:
+            pixels[i] = OFF
+    
     pixels.show()
-    # Increase or decrease to change the speed of the solid color change.
-    time.sleep(1)
-    pixels.fill(GREEN)
-    pixels.show()
-    time.sleep(1)
-    pixels.fill(BLUE)
-    pixels.show()
-    time.sleep(1)
-
-    color_chase(RED, 0.1)  # Increase the number to slow down the color chase
-    color_chase(YELLOW, 0.1)
-    color_chase(GREEN, 0.1)
-    color_chase(CYAN, 0.1)
-    color_chase(BLUE, 0.1)
-    color_chase(PURPLE, 0.1)
-
-    rainbow_cycle(0)  # Increase the number to slow down the rainbow
+    active = (active + 1) % NUM_PIXELS
+    time.sleep(0.01)
