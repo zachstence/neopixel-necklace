9b73453b3177624ca778c80283ff68808c383be8
Zachary Stence <zachstence@gmail.com>
2023-02-21 11:21:41 -0600

    feat: spinning rings

--- neopixel-necklace.ino
+++ neopixel-necklace.ino
@@ -1,119 +1,52 @@
-// SPDX-FileCopyrightText: 2018 Mikey Sklar for Adafruit Industries
-//
-// SPDX-License-Identifier: MIT
+#include "FastLED.h"
 
-#include <FastLED.h>
+#define NUM_LEDS 23
+#define LEDS_PIN 1
 
-#define LED_PIN      1   // which pin your pixels are connected to
-#define NUM_LEDS    23   // how many LEDs you have
-#define BRIGHTNESS  200   // 0-255, higher number is brighter. 
-#define SATURATION 255   // 0-255, 0 is pure white, 255 is fully saturated color
-#define SPEED       100   // How fast the colors move.  Higher numbers = faster motion
-#define STEPS        2   // How wide the bands of color are.  1 = more like a gradient, 10 = more like stripes
-#define BUTTON_PIN   2   // button is connected to pin 2 and GND
+CRGB _leds[NUM_LEDS];
 
-#define COLOR_ORDER GBR  // Try mixing up the letters (RGB, GBR, BRG, etc) for a whole new world of color combinations
+CRGBSet leds(_leds, NUM_LEDS);
+CRGBSet outer(leds(0, 15));
+CRGBSet center(leds(16, 16));
+CRGBSet inner(leds(17, 22));
 
-CRGB leds[NUM_LEDS];
-CRGBPalette16 currentPalette;
-CRGBPalette16 targetPalette( PartyColors_p );
-TBlendType    currentBlending;
-int ledMode = 5;
+auto outerColor = CHSV(40, 255, 255);
+auto innerColor = CHSV(100, 255, 255);
+auto centerColor = CHSV(160, 255, 255);
 
+void setup() { 
+    FastLED.addLeds<NEOPIXEL, LEDS_PIN>(leds, NUM_LEDS);
+    FastLED.setBrightness(70);
 
-//FastLED comes with several palettes pre-programmed.  I like purple a LOT, and so I added a custom purple palette.
-
-const TProgmemPalette16 PurpleColors_p PROGMEM =
-{
-  CRGB::Purple,
-  CRGB::Purple, 
-  CRGB::MidnightBlue,
-  CRGB::MidnightBlue,
-
-  CRGB::Purple,
-  CRGB::Purple,
-  CRGB::BlueViolet,
-  CRGB::BlueViolet,
-
-  CRGB::DarkTurquoise,
-  CRGB::DarkTurquoise,
-  CRGB::DarkBlue,
-  CRGB::DarkBlue,
-
-  CRGB::Purple,
-  CRGB::Purple,
-  CRGB::BlueViolet,
-  CRGB::BlueViolet
-};
-
-
-unsigned long keyPrevMillis = 0;
-const unsigned long keySampleIntervalMs = 25;
-byte longKeyPressCountMax = 80;    // 80 * 25 = 2000 ms
-byte longKeyPressCount = 0;
-
-byte prevKeyState = HIGH;         // button is active low
-
-void setup() {
-  delay( 2000 ); // power-up safety delay
-  FastLED.addLeds<WS2812B, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
-  FastLED.setBrightness(  BRIGHTNESS );
-  currentBlending =LINEARBLEND;
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
+    // Clear all LEDs
+    leds.fill_solid(CRGB::Black);
+    FastLED.show();
 }
+
+uint8_t c = 0;
+uint8_t dimBy = 70;
 
 void loop() {
+    for (auto i = 0; i < outer.len; i++) {
+        uint8_t v = max(0, outerColor.v - i * dimBy);
+        auto _i = (i + c) % outer.len;
+        auto color = CHSV(outerColor.h, outerColor.s, v);
+        outer[_i] = color;
+    }
 
-  byte currKeyState = digitalRead(BUTTON_PIN);
+    for (auto i = 0; i < inner.len; i++) {
+        uint8_t v = max(0, innerColor.v - i * dimBy);
+        auto _i = (i + c + 255) % inner.len;
+        auto color = CHSV(innerColor.h, innerColor.s, v);
+        inner[_i] = color;
+    }
 
-  if ((prevKeyState == LOW) && (currKeyState == HIGH)) {
-    shortKeyPress();
-  }
-  prevKeyState = currKeyState;
+    auto centerOffset = c % 30;
+    auto centerV = max(0, centerColor.v - centerOffset * 10);
+    auto color = CHSV(centerColor.h, centerColor.s, centerV);
+    center = color;
 
-  static uint8_t startIndex = 0;
-  startIndex = startIndex + 1; /* motion speed */
-
-  switch (ledMode) {
-
-  case 0:
-    currentPalette = HeatColors_p;    //Red & Yellow, Fire Colors
-    break;
-  case 1:
-    currentPalette = ForestColors_p;    //Foresty greens and yellows
-    break;
-  case 2: 
-    currentPalette = OceanColors_p;  //Oceans are pretty and filled with mermaids
-    break;
-  case 3: 
-    currentPalette = PurpleColors_p;  //My custom palette from above
-    break;
-  case 4:
-    currentPalette = RainbowColors_p;  //All the colors!
-    break;
-  case 5:
-    currentPalette = RainbowStripeColors_p;   //Rainbow stripes
-    break;      
-  case 6:
-    currentPalette = PartyColors_p; //All the colors except the greens, which make people look a bit washed out
-    break;
-  } 
-
-  FillLEDsFromPaletteColors( startIndex);
-  FastLED.show();
-  FastLED.delay(1000 / SPEED);  
-}
-
-void FillLEDsFromPaletteColors( uint8_t colorIndex) {
-  for( int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = ColorFromPalette( currentPalette, colorIndex, BRIGHTNESS, currentBlending);
-    colorIndex += STEPS;              
-  }
-}
-
-void shortKeyPress() {
-  ledMode++;
-  if (ledMode > 6) {
-    ledMode=0; 
-  }  
+    c--;
+    FastLED.show();
+    delay(50);
 }
